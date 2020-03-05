43e548d2861e87543ded8d75f28f7bec79c57a52
Chris Camfield <ccamfield@sagomini.com>
2020-03-05 11:44:02 -0500

    Created TouchTesting as touch observer

---
+++ TouchTest/Assets/Scripts/TouchTesting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SagoTouch;
+using Touch = SagoTouch.Touch;
+
+public class TouchTesting : MonoBehaviour, ISingleTouchObserver {
+
+
+#region Touch methods
+
+	public bool OnTouchBegan(Touch touch) {
+		return false;
+	}
+
+	public void OnTouchMoved(Touch touch) {
+	}
+
+	public void OnTouchEnded(Touch touch) {		
+	}
+
+	public void OnTouchCancelled(Touch touch) {	
+	}
+
+#endregion
+
+
+}
