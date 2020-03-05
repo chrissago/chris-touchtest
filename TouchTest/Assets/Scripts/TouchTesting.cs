6891574deaf2bf59a7395953d9fac96dc284e9a9
Chris Camfield <ccamfield@sagomini.com>
2020-03-05 11:48:02 -0500

    Register TouchTesting with TouchDispatcher

--- TouchTest/Assets/Scripts/TouchTesting.cs
+++ TouchTest/Assets/Scripts/TouchTesting.cs
@@ -7,6 +7,25 @@ using Touch = SagoTouch.Touch;
 public class TouchTesting : MonoBehaviour, ISingleTouchObserver {
 
 
+#region Monobehavior methods
+
+public void OnEnable() {
+	if (TouchDispatcher.Instance == null) {
+		return;
+	}
+	TouchDispatcher.Instance.Add(this, 0, true);
+}
+
+public void OnDisable() {
+	if (TouchDispatcher.Instance == null) {
+		return;
+	}
+	TouchDispatcher.Instance.Remove(this);
+}
+
+#endregion
+
+
 #region Touch methods
 
 	public bool OnTouchBegan(Touch touch) {
