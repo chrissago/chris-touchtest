88dad8e5322c2bb5a1bef08c4c924e800715f54f
Chris Camfield <ccamfield@sagomini.com>
2020-03-05 13:10:05 -0500

    Track (and only accept) one touch. Log the delta position each time OnTouchMoved() is called and the Touch.Position has changed

--- TouchTest/Assets/Scripts/TouchTesting.cs
+++ TouchTest/Assets/Scripts/TouchTesting.cs
@@ -7,6 +7,13 @@ using Touch = SagoTouch.Touch;
 public class TouchTesting : MonoBehaviour, ISingleTouchObserver {
 
 
+#region Fields
+
+private Touch m_Current;
+private Vector2 m_CurrentLastPos;
+
+#endregion
+
 #region Monobehavior methods
 
 public void OnEnable() {
@@ -29,16 +36,38 @@ public void OnDisable() {
 #region Touch methods
 
 	public bool OnTouchBegan(Touch touch) {
+		if (m_Current == null)
+		{
+			Debug.Log("Touch Began");
+			m_Current = touch;
+			m_CurrentLastPos = m_Current.Position;
+			return true;
+		}
 		return false;
 	}
 
 	public void OnTouchMoved(Touch touch) {
+		if (touch == m_Current && touch.Position != m_CurrentLastPos)
+		{
+			Debug.Log(touch.Position - m_CurrentLastPos);
+			m_CurrentLastPos = m_Current.Position;
+		}
 	}
 
 	public void OnTouchEnded(Touch touch) {		
+		Debug.Log("Touch ended");
+		if (touch == m_Current)
+		{
+			m_Current = null;
+		}
 	}
 
 	public void OnTouchCancelled(Touch touch) {	
+		Debug.Log("Touch cancelled");
+		if (touch == m_Current)
+		{
+			m_Current = null;
+		}
 	}
 
 #endregion
